b849d65d8a9237c07d03c748bd027733a6ed3480
corbin <stramapatryk@gmail.com>
2022-05-22 17:38:00 +0200

    Replaced get_random_from_list with random.choices. Replaced randomize_placement with random.sample. Made main function clearer.

--- main.py
+++ main.py
@@ -2,39 +2,26 @@ import string
 import random
 
 
-def get_random_from_list(count=0, data_list=[]):
-    if len(data_list) == 0:
-        return []
-
-    picked = []
-
-    while count:
-        picked.append(
-            data_list[random.randint(0, len(data_list) - 1)])
-        count -= 1
-
-    print(picked)
-    return picked
-
-
 def get_letters(count=0, big_letters=True):
+    if count < 0:
+        return [];
+
     letters_data = string.ascii_lowercase
     if big_letters:
         letters_data += string.ascii_uppercase
 
-    return get_random_from_list(count, letters_data)
+    return random.choices(letters_data, k=count)
 
 
 def get_numbers(count=0, punctuation=True):
-    numbers_data = string.ascii_letters
+    if count < 0:
+        return [];
+
+    numbers_data = string.digits
     if punctuation:
         numbers_data += string.punctuation
 
-    return get_random_from_list(count, numbers_data)
-
-
-def randomize_placement(letters=[], numbers=[]):
-    return "password"
+    return random.choices(numbers_data, k=count)
 
 
 def password_dont_have_words(password=""):
@@ -67,20 +54,24 @@ def main():
     print("Welcome to password generator",
           "Specify your password", sep="\n")
 
+    data_list=[]
+
     amount_of_letters = get_number_input("How many letters do you want?")
     want_big_letters = get_bool_input("Do you want big letters?")
-    letters = get_letters(amount_of_letters,want_big_letters)
+    data_list += get_letters(amount_of_letters,want_big_letters)
+    print(data_list)
 
     amount_of_numbers = get_number_input("How many numbers do you want")
     want_punctuation = get_bool_input("Do you want punctuation")
-    numbers = get_numbers(amount_of_numbers, want_punctuation)
+    data_list += get_numbers(amount_of_numbers, want_punctuation)
+    print(data_list)
 
     while True:
-        password = randomize_placement(letters, numbers)
+        password = random.sample(data_list, k=len(data_list))
         if password_dont_have_words(password):
             break
 
-    print("Your password is:", password)
+    print("Your password is:", "".join(password))
 
 
 random.seed()
